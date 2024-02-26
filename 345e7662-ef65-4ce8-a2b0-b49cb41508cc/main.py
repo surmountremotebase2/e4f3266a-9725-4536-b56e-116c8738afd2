94bdfa176696fbced8bb5a8bddb8b93ee1fbfc9b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-02-26 06:43:24 -0500

    User Hassan Raza saved changes to main.py at 02/26/2024, 11:43:24 UTC time.

--- 345e7662-ef65-4ce8-a2b0-b49cb41508cc/main.py
+++ 345e7662-ef65-4ce8-a2b0-b49cb41508cc/main.py
@@ -15,14 +15,12 @@ class TradingStrategy(Strategy):
         data = data_functions["ohlcv"]
 
         spy_20_ma = SMA("SPY", data, 20)
-        spy_10_ma = SMA("SPY", data, 10)
-        spy_10_rsi = RSI("SPY", data, 10)
 
         if None in [spy_20_ma, spy_10_ma, spy_10_rsi]:
             return None
 
         spy_stake = 0
         if spy_10_rsi[-1]<60 and spy_10_ma[-1]>spy_20_ma[-1]:
-            spy_stake = 1
+
 
         return TargetAllocation({"SPY": spy_stake})
