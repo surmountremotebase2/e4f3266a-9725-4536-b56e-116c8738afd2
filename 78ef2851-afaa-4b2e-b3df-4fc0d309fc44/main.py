f155b29ed0fb421955e16cc5423bb8b6eef6e16c
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-02-27 03:31:08 -0500

    User Hassan Raza saved changes to main.py at 02/27/2024, 08:31:07 UTC time.

--- 78ef2851-afaa-4b2e-b3df-4fc0d309fc44/main.py
+++ 78ef2851-afaa-4b2e-b3df-4fc0d309fc44/main.py
@@ -9,20 +9,20 @@ class TradingStrategy(Strategy):
 
     @property
     def interval(self):
-        return "1hour"
+        return "2hour"
 
     def run(self, data_functions):
         data = data_functions["ohlcv"]
 
-        spy_20_ma = SMA("SPY", data, 20)
-        spy_10_ma = SMA("SPY", data, 10)
-        spy_10_rsi = RSI("SPY", data, 10)
+        spy_20_ma = SMA("SPY", data, 50)
+        spy_10_ma = SMA("SPY", data, 50)
+        spy_10_rsi = RSI("SPY", data, 50)
 
         if None in [spy_20_ma, spy_10_ma, spy_10_rsi]:
             return None
 
         spy_stake = 0
-        if spy_10_rsi[-1]<60 and spy_10_ma[-1]>spy_20_ma[-1]:
+        if spy_10_rsi[-1]<80 and spy_10_ma[-1]>spy_20_ma[-1]:
             spy_stake = 1
 
         return TargetAllocation({"SPY": spy_stake})
